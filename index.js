e7038da680ab308e1b9f9e3166f471c18363674f
Aroonima Mohile <aroonimamohile02@gmail.com>
2021-09-14 12:51:56 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    alert('I was clicked!');
+    const input = document.getElementById('input');
+    input.addEventListener('click', addingEventListener);
+    
+    const six = document.getElementById('divSix');
+    divSix.addEventListener('click', addingEventListener);
 }
+
+addingEventListener();
